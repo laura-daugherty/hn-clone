b4a151e7496da3b0c42828af474e650b9227c12d
Laura Daugherty <lauradaugherty6@gmail.com>
2020-09-10 14:34:19 -0700

    fetching top articles

--- hn-clone/src/App.js
+++ hn-clone/src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import logo from './logo.svg';
+import axios from "axios";
 import './App.css';
 
 function App() {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const [curStory, setCurStory] = useState("");
+
+
+  useEffect(() => {
+    console.log("useEffect fetching Data")
+    axios
+      .get("https://hacker-news.firebaseio.com/v0/topstories.json?print=pretty")
+      .then(result => setData(result.data))
+      .catch(err => setError("THERES AN ERROR!"));
+  }, [] );
+    console.log("data", data)
+
+  // function displayData() {
+  //       let i
+  //       for (i = 0; i<data.length; i++) {
+  //         let curData = data[i]
+  //         console.log("fetching ind articles")
+  //         axios
+  //           .get(`https://hacker-news.firebaseio.com/v0/${curData}.json?print=pretty`)
+  //           .then(result => console.log("result", result))
+  //           .catch(err => setError("THERES AN ERROR!"));
+  //       }
+  //       console.log("curStory", curStory)
+  //   }
+
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {displayData()}
+      {error}
     </div>
   );
 }
