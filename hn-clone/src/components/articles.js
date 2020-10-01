05b9f589589ed56444ee8cdfd333822eee82f980
Laura Daugherty <lauradaugherty6@gmail.com>
2020-10-01 15:42:00 -0700

    articles

--- hn-clone/src/components/articles.js
+++ hn-clone/src/components/articles.js
@@ -1,22 +1,33 @@
 import React, {useState, useEffect} from "react"
 import Story from "./story"
 const Articles = (props) => {
+  let index=props.more*30
 
+  function setMore() {
+    let more = props.more
+    props.setMore(more += 1)
+    console.log("newMore", props.more)
+  }
   return (
-    <div>
-      <div className="stories_container">
-        {props.stories.map((story, index) => {     
+    <div className="stories_container">
+      <div >
+        {props.stories.map((story) => { 
+          index += 1
           return (
             <Story
               key = {story.id}
               story = {story}
               index = {index}
               setStory = {props.setStory}
+              more = {props.more}
               />
           )
         })}
         
       </div>
+      <button onClick = {() => {setMore()}}>
+        More
+      </button>
 
     </div>);      
 
