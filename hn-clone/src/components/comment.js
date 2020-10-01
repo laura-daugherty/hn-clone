99b941933de15fc86554e7534a4c510b9bf284fa
Laura Daugherty <lauradaugherty6@gmail.com>
2020-10-01 15:41:54 -0700

    comment

--- hn-clone/src/components/comment.js
+++ hn-clone/src/components/comment.js
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react"
 import axios from "axios";
+import img from "./images/equilateral-triangle-png-triangles-are-made-to-order.png"
+
 
 const Comment = (props) => {
 
@@ -13,40 +15,42 @@ const Comment = (props) => {
 
 
   console.log("comment", comment)
-
-  // return (
-  //   // display()
-  //   <div>
-  //     comment
-  //     {comment.data.text}
-  //   </div>
-  // );      
-
-  function display() {
-    if (comment && comment.data) {
+  function timeFromCreation(time) {
+    var todaysDate = Math.round((new Date()).getTime() / 1000)
+    var dif = (todaysDate - time)
+    var hours = Math.floor((dif/60)/60)
+    if (hours === 1) {
       return (
-        <div>
-          <div>
-            {comment.data.text}
-
-          </div>
-        </div>
-
+        <p>{hours} hour ago</p>
       )
     } else {
       return (
-        <div>
-          Fetching comments
-        </div>
+        <p>{hours} hours ago</p>
       )
     }
+  }
+  
+  if (comment && comment.data) {
+    const decodedText = decodeURIComponent(comment.data.text)
+
+    return (
+      <div className="comment">
+        <div className="name_and_time">
+          <img src={img} alt="upward triangle" className="arrow"/>
+          <p className="name_and_time_one">{comment.data.by}</p>
+          <p>{timeFromCreation(comment.data.time)}</p>
+        </div>
+        <div 
+          className="innerHTML" 
+          dangerouslySetInnerHTML={{ __html: decodedText }}/>
+        </div>
+
+    )
+  } else {
+    return (
+      <div>
+      </div>
+    )
   }  
-
-  return (
-    <div>
-      <div>{display()}</div>
-    </div>
-
-  )
 }
 export default Comment;
